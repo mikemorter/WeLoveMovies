383a7e012eb43df56247d6b5f3ad0d505c593a7e
Mike Morter <mikemorter@gmail.com>
2021-05-29 11:55:01 -0600

    second commit

---
+++ src/theaters/theaters.controller.js
@@ -0,0 +1,17 @@
+const service = require("./theaters.services");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function list(req, res) {
+  const movieId = req.params.movie_id;
+  let data;
+  if (movieId) {
+    data = await service.listTheatersByMovieId(movieId);
+  } else {
+    data = await service.list();
+  }
+  res.json({ data });
+}
+
+module.exports = {
+  list: asyncErrorBoundary(list),
+};
